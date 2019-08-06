05afe50c06aa27bddd690dfc30844aea84dd5e6d
Thiago <43359285+thiagorino@users.noreply.github.com>
2019-08-06 20:44:27 -0300

    Update primeiros_passos.cpp

--- primeiros_passos.cpp
+++ primeiros_passos.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 using namespace std;
+void altera (int k){
+  k+=1;
+  cout << "\n\nX alterado pelo método = " <<
+}
 int main() {
   //HelloWord
   string olamundo("Hello Word!!!!");
@@ -10,15 +14,37 @@ int main() {
   while(x>=0 and x<=20){
     cout << '\n';
     cout << x;
-    x+=1;
+      }
+x+=1;
   }
   //Contando em forma decrescente com for
   if (x>20){
   x = x - 1;
-  }
   cout << "\n\n\n";
   int i;
   for(i = x; i >=0;i--){
     cout << '\n' << i;
   }
+
+  int a = 0;
+  //Pedindo um valor para o usuário
+  cout << "\n\t Insira um valor -> ";
+  cin >> a;
+
+  cout << '\n' << a;
+
+  while (a < 100){
+    cout << "--------\n";
+    cout << a + 1;
+    a+=1;
+  }
+
+  //Tipos primitivos
+  /* int, long, float/double,char */
+
+  //Tipos "objeto"
+  /* */
+
+   
+
 }
