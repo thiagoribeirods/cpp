5ba9c918c2a8fd93a2d4703d47663b19694b181a
Thiago <43359285+thiagorino@users.noreply.github.com>
2019-11-12 20:28:46 -0300

    Update GameOfLife.c

--- GameOfLife.c
+++ GameOfLife.c
@@ -1,125 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-int mapa[15][15];
-int vetProximaGeracao[15][15];
+/*
+	Autores: - Breno Fernandes 41890590 - Gustavo Aveloes 41882318 - Thiago Ribeiro 41891147
+*/
 
-void printMapa(int mapa[15][15])
-{	
+int mapa[25][25] = { 0 };
+
+int vetProximaGeracao[25][25];
+
+void printMapa()
+{
 	int i, j;
 	//exibindo mapa
-	for(i = 0; i < 15; i++)
-    {
-        printf("[ ");
-        for(j = 0; j < 15; j++)
-        {
-            printf("%i  ", mapa[i][j]);
-        }
-        printf(" ]");
-        printf("\n");
-    }
+	for (i = 0; i < 25; i++) {
+		printf("[ ");
+		for (j = 0; j < 25; j++) {
+			printf("%i  ", mapa[i][j]);
+		}
+		printf(" ]");
+		printf("\n");
+	}
 }
 
-void proximaGeracao(int mapa[15][15], int quantidadeDeGeracoes)
+void limpaVetProximaGeracao()
 {
-	int a,b,c,d, e;
+	int i, j;
+	for (i = 0; i < 25; i++) {
+		for (j = 0; j < 25; j++) {
+			vetProximaGeracao[i][j] = 0;
+		}
+	}
+}
+
+void proximaGeracao(int quantidadeDeGeracoes)
+{
+	int a, b, c, d, e;
 	e = 0;
-	while(e < quantidadeDeGeracoes)
-	{
-		for(a = 0; a < 15; a++)
-		{
-			for(b = 0; b < 15; b++)
-			{
-				int vivos = 0;
-				for(c = -1; c < 2; c++)
-				{
-					for(d = -1; d < 2; d++)
-					{
-						vivos+=mapa[a+c][b+d];
+	int vivos;
+	while (e < quantidadeDeGeracoes) {
+		//Preeche o vetor auxiliar com zeros
+		limpaVetProximaGeracao();
+
+		for (a = 0; a < 25; a++) {
+			for (b = 0; b < 25; b++) {
+				vivos = 0;
+				for (c = -1; c < 2; c++) {
+					for (d = -1; d < 2; d++) {
+						vivos += mapa[a + c][b + d];
 					}
 				}
 				vivos -= mapa[a][b];
+
 				//a celula morre por solidão
-				if(mapa[a][b] == 1 && vivos < 2)
-				{
+				if (mapa[a][b] == 1 && vivos < 2) {
 					vetProximaGeracao[a][b] = 0;
-					
 				}
 				//condição para nascer uma célula
-				else if(mapa[a][b] == 0 && vivos == 3)
-				{
+				else if (mapa[a][b] == 0 && vivos == 3) {
 					vetProximaGeracao[a][b] = 1;
 				}
 				//morre de superpopulação
-				else if(mapa[a][b] == 1 && vivos > 3)
-				{
+				else if (mapa[a][b] == 1 && vivos > 3) {
 					vetProximaGeracao[a][b] = 0;
 				}
 				//termina de preencher o mapa
-				else
-				{
-					vetProximaGeracao[a][b] = mapa[a][b];	
+				else {
+					vetProximaGeracao[a][b] = mapa[a][b];
 				}
 			}
 		}
 		int i, j;
 		//exibindo mapa
-		for(i = 0; i < 15; i++)
-    	{
-        	printf("[ ");
-        	for(j = 0; j < 15; j++)
-        	{
-            	printf("%i  ", vetProximaGeracao[i][j]);
-        	}
-        	printf(" ]");
-        	printf("\n");
-    	}
+		for (i = 0; i < 25; i++) {
+			printf("[ ");
+			for (j = 0; j < 25; j++) {
+				printf("%i  ", vetProximaGeracao[i][j]);
+			}
+			printf(" ]");
+			printf("\n");
+		}
 		printf("\n");
-		sleep(2);
-		e++;	
+
+		//Atualiza mapa com a nova geracao
+		memcpy(mapa, vetProximaGeracao, 25 * 25 * sizeof(int));
+		//sleep(2);
+		e++;
 	}
 }
 
-int main(int argc, char *argv[]) {
-    printf("\tGame Of Life - Conway");
-    int i,j,k,l,x,y,quantidade,geracoes;
-    printf("\n Insira quantas celula voce quer povoar -> ");
-    scanf("%i",&quantidade);
-    l=0;
-    while(l<quantidade)
-    {
-        printf("\nInsira as coordenadas desejadas -> ");
-        printf("\nEixo X -> ");
-        scanf("%i", &x);
-        printf("\nEixo Y -> ");
-        scanf("%i", &y);
-        for(i=0;i<15;i++)
-        {
-            for(j=0;j<15;j++)
-            {
-                if(i==x && j==y)
-                {
-                    mapa[i][j]=1;
-                }
-                else
-                {
-                	if(mapa[i][j]==1)
-                	{
-                		mapa[i][j]=1;
-					}
-					else
-					{
-						mapa[i][j]=0;
-					}
-				}  
-            }
-        }
-        l++;     
-    }
-    printMapa(mapa);
-    printf("\nInsira o numero de geracoes -> ");
-    scanf("%i",&geracoes);
-	proximaGeracao(mapa,geracoes);
-	system("pause");
-    return 0;
+int main()
+{
+	printf("\tGame Of Life - Conway");
+	int i, j, k, l, x, y, quantidade, geracoes;
+	printf("\n Insira quantas celula voce quer povoar -> ");
+	scanf("%i", &quantidade);
+	l = 0;
+	while (l < quantidade) {
+		printf("\nInsira as coordenadas desejadas -> ");
+		printf("\nEixo X -> ");
+		scanf("%i", &x);
+
+		printf("\nEixo Y -> ");
+		scanf("%i", &y);
+
+		mapa[x][y] = 1;
+		l++;
+	}
+
+	printMapa();
+	printf("\nInsira o numero de geracoes -> ");
+	scanf("%i", &geracoes);
+	proximaGeracao(geracoes);
+
+	return 0;
 }
